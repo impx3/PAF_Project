c4f64a1dacda6a5f6d1c65c8c2d1083752153106
Piyara Perera <jappp020618@gmail.com>
2025-03-20 20:06:48 +0530

    Project initialization

---
+++ skill-sharing/src/main/java/com/skillshare/skill_sharing/SkillSharingApplication.java
@@ -0,0 +1,13 @@
+package com.skillshare.skill_sharing;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SkillSharingApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SkillSharingApplication.class, args);
+	}
+
+}
---
+++ skill-sharing/src/test/java/com/skillshare/skill_sharing/SkillSharingApplicationTests.java
@@ -0,0 +1,13 @@
+package com.skillshare.skill_sharing;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SkillSharingApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
