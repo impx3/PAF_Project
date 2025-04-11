3f2d99aaf210c9b55470e1d0c31523d815c616ea
pramudithamendis <134496244+pramudithamendis@users.noreply.github.com>
2025-04-11 19:50:17 +0530

    Adding permission to images mapping

--- backend/src/main/java/com/paf/chop/backend/configs/SecurityConfig.java
+++ backend/src/main/java/com/paf/chop/backend/configs/SecurityConfig.java
@@ -32,6 +32,7 @@ public class SecurityConfig {
                         .requestMatchers("/api/auth/*").permitAll()
                         .requestMatchers("/api/posts*").permitAll()
                         .requestMatchers("/api/posts/*").permitAll()
+                        .requestMatchers("/images/*").permitAll()
                         .anyRequest().authenticated()
                 )
                 .sessionManagement(session -> session
