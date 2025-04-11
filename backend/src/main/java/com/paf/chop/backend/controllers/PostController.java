8ce00e127849ef92e66c1ebc0d9bfc4a27959954
pramudithamendis <134496244+pramudithamendis@users.noreply.github.com>
2025-04-11 19:15:33 +0530

    Created PostController and FileStorageService

---
+++ backend/src/main/java/com/paf/chop/backend/controllers/PostController.java
@@ -0,0 +1,142 @@
+package com.paf.chop.backend.controllers;
+
+import com.paf.chop.backend.models.Post;
+import com.paf.chop.backend.services.PostService;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+	
+import com.paf.chop.backend.services.FileStorageService;
+
+import org.springframework.web.multipart.MultipartFile;
+
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.UUID;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+@CrossOrigin(origins = "http://localhost:3000/", maxAge = 3600)
+@RestController
+@RequestMapping("/api/posts")
+public class PostController {
+
+    @Autowired
+    private PostService postService;
+
+    	
+    @Autowired
+    private FileStorageService fileStorageService;
+
+
+    @GetMapping("/home")
+    public String HomeEndpoint(){
+        return "Homee at Postcontroller";
+    }
+
+    @PostMapping
+    public ResponseEntity<Post> createPost(
+    @RequestParam("title") String title,
+    @RequestParam("content") String content,
+    @RequestParam(value = "image", required = false) MultipartFile image) throws IOException {
+    
+    System.out.println(title+"hereyrer");
+    System.out.println(content+"hereyrer");
+    System.out.println(image+"hereyrer");
+
+
+    String imageUrl = image != null ? fileStorageService.storeFile(image) : null;
+    
+    Post post = new Post();
+    post.setTitle(title);
+    post.setContent(content);
+    post.setImageUrl(imageUrl);
+    System.out.println("\n\n\n\n\n"+post);
+        return ResponseEntity.ok(postService.createPost(post));
+    }
+
+
+    @GetMapping("/")
+    public String HomeEndPoint(){
+        return "Home";
+    }
+
+
+    @PostMapping("/multi")
+    public ResponseEntity<Post> createPostWithMultipleImages(
+            @RequestParam("title") String title,
+            @RequestParam("content") String content,
+            @RequestParam("images") List<MultipartFile> images) throws IOException {
+
+                System.out.println(title+"hereyrer");
+                System.out.println(content+"hereyrer");
+                // System.out.println(images+"hereyrer");
+
+        Post post = new Post();
+        post.setTitle(title);
+        post.setContent(content);
+        System.out.println("\n\n\n\n\n"+post);
+
+        List<String> savedImageFilenames = fileStorageService.saveFiles(images);
+        
+        System.out.println("nameszzz" + savedImageFilenames.get(0));
+        post.setImageUrl(String.join(",", savedImageFilenames));
+    
+        return ResponseEntity.ok(postService.createPost(post));
+
+        
+        }
+
+
+
+
+    @PutMapping("/{id}")
+    public ResponseEntity<Post> updatePost(
+    @PathVariable Long id
+    , @RequestParam("title") String title
+    , @RequestParam("content") String content
+    , @RequestParam(value = "image", required = false) MultipartFile image
+    , @RequestParam("imageURLinCaseUserDidnotREUPLOAD") String imageurlincase___
+    ) throws IOException {
+    System.out.println(imageurlincase___ + " incase thing");
+    Pattern pattern = Pattern.compile(".png", Pattern.CASE_INSENSITIVE);
+    Matcher matcher = pattern.matcher(imageurlincase___);
+    boolean matchFound = matcher.find();
+    if(matchFound) {
+        System.out.println("Match found");
+      } else {
+        System.out.println("Match not found");
+    }
+
+    String imageUrl = image != null ? fileStorageService.storeFile(image) : imageurlincase___;
+    System.out.println(imageUrl+ "hey");
+    Post post = new Post();
+    post.setId(id);
+    post.setTitle(title);
+    post.setContent(content);
+    post.setImageUrl(imageUrl);
+
+        return ResponseEntity.ok(postService.updatePost(id, post));
+    }
+
+    @DeleteMapping("/{id}")
+    public ResponseEntity<String> deletePost(@PathVariable Long id) {
+        postService.deletePost(id);
+        return ResponseEntity.ok("Post deleted successfully");
+    }
+
+    @GetMapping
+    public ResponseEntity<List<Post>> getAllPosts() {
+        return ResponseEntity.ok(postService.getAllPosts());
+    }
+
+    @GetMapping("/{id}")
+    public ResponseEntity<Post> getPostById(@PathVariable Long id) {
+        return ResponseEntity.ok(postService.getPostById(id));
+    }
+}
---
+++ backend/src/main/java/com/paf/chop/backend/services/FileStorageService.java
@@ -0,0 +1,97 @@
+package com.paf.chop.backend.services;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Service;
+import org.springframework.web.multipart.MultipartFile;
+
+import java.io.File;
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.util.UUID;
+
+import java.nio.file.*;
+import java.util.ArrayList;
+import java.util.List;
+
+
+import java.util.logging.Logger;
+
+
+@Service
+public class FileStorageService {
+
+    @Value("${file.upload-dir}")
+    private String uploadDir;
+
+                private static final Logger LOGGER = Logger.getLogger(FileStorageService.class.getName());
+
+
+    public String storeFile(MultipartFile file) throws IOException {
+         // making sure  directory exists
+         Path uploadPath = Paths.get(uploadDir);
+         if (!Files.exists(uploadPath)) {
+             Files.createDirectories(uploadPath);
+         }
+         // extract the original file extension
+         String originalFilename = file.getOriginalFilename();
+
+        // ensure originalFilename is not null and contains a dot to get file extension
+        String fileExtension = "";
+        if (originalFilename != null && originalFilename.lastIndexOf(".") != -1) {
+            fileExtension = originalFilename.substring(originalFilename.lastIndexOf("."));
+        } else {
+            LOGGER.warning("No file extension found!");
+        }
+        // fileExtension = ".png";
+         // generate a unique file name and include the extension
+         String fileName = UUID.randomUUID().toString() + fileExtension;
+ 
+
+         // create file path
+         Path filePath = uploadPath.resolve(fileName);
+ 
+         // store the file
+         Files.write(filePath, file.getBytes());
+ 
+         // return the stored file path
+         return filePath.toString();
+    }
+
+    
+    //  to save multiple image files and returns their filenames
+     
+    public List<String> saveFiles(List<MultipartFile> files) throws IOException {
+        List<String> filenames = new ArrayList<>();
+
+        Path uploadPath = Paths.get(uploadDir);
+        if (!Files.exists(uploadPath)) {
+            Files.createDirectories(uploadPath);
+        }
+
+        for (MultipartFile file : files) {
+            if (!file.isEmpty()) {
+                String originalFilename = file.getOriginalFilename();
+                String extension = "";
+
+                if (originalFilename != null && originalFilename.contains(".")) {
+                    extension = originalFilename.substring(originalFilename.lastIndexOf('.'));
+                }
+
+                String uniqueFilename = UUID.randomUUID() + extension;
+
+
+                Path destination = uploadPath.resolve(uniqueFilename);
+                
+                // Files.write(destination, file.getBytes());
+                 Files.write(destination, file.getBytes());
+
+                filenames.add(uniqueFilename);
+            }
+        }
+
+        return filenames;
+    }
+
+}
