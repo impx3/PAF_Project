9a60fa44d72b2978dfa44c51b385ac11189e5e97
Piyara Perera <jappp020618@gmail.com>
2025-04-13 12:27:11 +0530

    Added main frontend files

---
+++ frontend/taliwind.config.js
@@ -0,0 +1,16 @@
+module.exports = {
+  content: ['./src/**/*.{js,jsx,ts,tsx}'],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+};
+
+
+//cd frontend
+//npm install
+//npm start
+//This will open http://localhost:3000 in the browser.
+
+//cd backend
+//mvn spring-boot:run
