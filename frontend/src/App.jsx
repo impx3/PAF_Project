143f7caf57af4640749e88eba9e5561c793421f0
Piyara Perera <jappp020618@gmail.com>
2025-04-16 14:06:43 +0530

    Added main layout

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,41 +1,40 @@
+import React from 'react';
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
-import Profile from './pages/Profile';
-import Followers from './pages/Followers';
-import Home from './pages/Home';
-import DeleteAccount from './pages/DeleteAccount';
-import Login from './pages/Login';
-import Register from './pages/Register';
-import LandingPage from './pages/LandingPage';
-import PrivateRoute from './routes/PrivateRoute';
-import { AuthProvider } from './context/AuthContext';
-import LeftPanel from './components/LeftPanel';
-import Header from './components/Header';
-import EditProfile from './pages/EditProfile';
-import UserList from './pages/UserList';
 import { ToastContainer } from 'react-toastify';
 import 'react-toastify/dist/ReactToastify.css';
+
+import { AuthProvider } from './context/AuthContext';
+import PrivateRoute from './components/PrivateRoute';
+import MainLayout from './layouts/MainLayout';
+
+import Login from './pages/Login';
+import Register from './pages/Register';
+import Home from './pages/Home';
+import Profile from './pages/Profile';
+import Followers from './pages/Followers';
+import EditProfile from './pages/EditProfile';
+import DeleteAccount from './pages/DeleteAccount';
+import LandingPage from './pages/LandingPage';
+import UserList from './pages/UserList';
 
 function App() {
   return (
     <AuthProvider>
       <BrowserRouter>
-	{/* <div className="flex"> */}
-         <LeftPanel />
-        <div className="ml-64 flex-grow">
-          <Header />
-          <Routes>
-            <Route path="/login" element={<Login />} />
-            <Route path="/register" element={<Register />} />
-            <Route path="/home" element={<PrivateRoute><Home /></PrivateRoute>} />
-            <Route path="/profile/:id" element={<PrivateRoute><Profile /></PrivateRoute>} />
-            <Route path="/followers" element={<PrivateRoute><Followers /></PrivateRoute>} />
-            <Route path="/edit-profile" element={<PrivateRoute><EditProfile /></PrivateRoute>} />
-            <Route path="/delete-account" element={<PrivateRoute><DeleteAccount /></PrivateRoute>} />
-            <Route path="/" element={<LandingPage />} />
-            <Route path="/explore" element={<UserList />} />
-          </Routes>
-        </div>
-       {/* </div> */}
+      <Routes>
+          {/* Public Routes */}
+          <Route path="/" element={<LandingPage />} />
+          <Route path="/login" element={<Login />} />
+          <Route path="/register" element={<Register />} />
+
+          {/* Protected Routes */}
+          <Route path="/home" element={<PrivateRoute><MainLayout><Home /></MainLayout></PrivateRoute>} />
+          <Route path="/profile/:id" element={<PrivateRoute><MainLayout><Profile /></MainLayout></PrivateRoute>} />
+          <Route path="/followers" element={<PrivateRoute><MainLayout><Followers /></MainLayout></PrivateRoute>} />
+          <Route path="/edit-profile" element={<PrivateRoute><MainLayout><EditProfile /></MainLayout></PrivateRoute>} />
+          <Route path="/delete-account" element={<PrivateRoute><MainLayout><DeleteAccount /></MainLayout></PrivateRoute>} />
+          <Route path="/explore" element={<PrivateRoute><MainLayout><UserList /></MainLayout></PrivateRoute>} />
+        </Routes>
       </BrowserRouter>
       <ToastContainer position="top-right" autoClose={3000} hideProgressBar newestOnTop />
     </AuthProvider>
