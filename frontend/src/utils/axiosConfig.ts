436fcab0039544ac0af5a7e79e0f767d4b577bb5
Piyara Perera <jappp020618@gmail.com>
2025-04-20 19:48:21 +0530

    Added url with env file

--- frontend/src/utils/axiosConfig.ts
+++ frontend/src/utils/axiosConfig.ts
@@ -1,10 +1,10 @@
-import axios, { AxiosRequestConfig } from 'axios';
+import axios, { InternalAxiosRequestConfig } from 'axios';
 
 const api = axios.create({
-  baseURL: 'http://localhost:8080/api',
+  baseURL: import.meta.env.VITE_API_URL,
 });
 
-api.interceptors.request.use((config: AxiosRequestConfig) => {
+api.interceptors.request.use((config: InternalAxiosRequestConfig) => {
   const token = localStorage.getItem('token');
   if (token && config.headers) {
     config.headers.Authorization = `Bearer ${token}`;
