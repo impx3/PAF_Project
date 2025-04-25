d1b04db34402d41ffb549df011940b280b359cda
YashodharaSandeepani <it22327062@my.sliit.lk>
2025-04-25 19:49:01 +0530

    Fix: Fix the dependency issue realted java file and fix the email issue related to userrepository

--- backend/src/main/java/com/paf/chop/backend/repositories/UserRepository.java
+++ backend/src/main/java/com/paf/chop/backend/repositories/UserRepository.java
@@ -9,12 +9,12 @@ import org.springframework.stereotype.Repository;
 @Repository
 public interface UserRepository extends JpaRepository<User, Long> {
 
-
     @Query("SELECT u FROM User u WHERE u.username = :username")
     User findByUsername(@Param("username") String username);
 
+    @Query("SELECT u FROM User u WHERE u.email = :email")
+    User findByEmail(@Param("email") String email);
+
     @Query("SELECT COUNT(u) > 0 FROM User u WHERE u.username = :username OR u.email = :email")
     boolean isUserExistByUsernameOrEmail(@Param("username") String username, @Param("email") String email);
-
-
 }
