47ea743b6ca254be019369088c3df485456f1863
pramudithamendis <134496244+pramudithamendis@users.noreply.github.com>
2025-05-05 13:23:32 +0530

    Solving cors

--- backend/src/main/java/com/paf/chop/backend/configs/SecurityConfig.java
+++ backend/src/main/java/com/paf/chop/backend/configs/SecurityConfig.java
@@ -32,6 +32,7 @@ public class SecurityConfig {
     public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
         http
                 .csrf(AbstractHttpConfigurer::disable)
+                .cors(withDefaults())
                 .authorizeHttpRequests(auth -> auth
 
                         .requestMatchers("/api/auth/*").permitAll()
